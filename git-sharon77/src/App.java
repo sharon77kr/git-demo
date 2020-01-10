aa8bc9f958806cbd555d3d30c9c10bc667e5c22c
goott6 <goott6@goott6-PC>
2020-01-10 15:30:33 +0900

    정일2

--- git-sharon77/src/App.java
+++ git-sharon77/src/App.java
@@ -3,42 +3,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		System.out.println("존잘남 박정일 짱!");
-		System.out.println("sharon77");
-		System.out.println("악!악!악!");
-		System.out.println("같이 포르쉐 사러가요111");
-		System.out.println("I don't give a fuck, bitch!!");
-
 		
-		
-		
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		
-		
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		System.out.println("I don't give a fuck, bitch!!");
-		
-		
-		
-		
-		
-
 	}
 
 }
